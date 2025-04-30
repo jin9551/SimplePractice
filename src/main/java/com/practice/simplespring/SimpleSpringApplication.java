09db2df431b7181cb3ff70fe5b2f47141c415b9d
jin9551 <wlsgk9551@gmail.com>
2025-04-30 22:30:29 +0900

    simple practice set up

---
+++ src/main/java/com/practice/simplespring/SimpleSpringApplication.java
@@ -0,0 +1,14 @@
+package com.practice.simplespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SimpleSpringApplication.class, args);
+    }
+
+}
